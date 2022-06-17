0fd4886d21a416ce972827606692cad4413c13c4
iu <iu@163.com>
2022-06-17 10:40:21 +0800

    iu  完善兑换列表

--- loveGame/uniCloud-tcb/cloudfunctions/get_exchange_list/index.js
+++ loveGame/uniCloud-tcb/cloudfunctions/get_exchange_list/index.js
@@ -1,5 +1,20 @@
 'use strict';
+const db = uniCloud.database()
 exports.main = async (event, context) => {
-	const res = await db.collection('exchange_list').get()
-	return res
+	let callback = {};
+	const dbCmd = db.command;
+	// 获取兑换列表
+	const collection = db.collection('task_day');
+	db.collection('exchange_list')
+	.get()
+	.then((res)=>{
+		// 查询当前对象 兑换的商品
+		callback = {
+			data:res.data,
+			mesg: 'success',
+			code: 200,
+		};
+	})
+	
+	return callback;
 };
